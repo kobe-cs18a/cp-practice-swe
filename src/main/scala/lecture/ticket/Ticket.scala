98dc36a931b008a54007882df6ba2171a70cf228
TakehideSoh <takehide.soh@gmail.com>
2024-12-15 16:23:49 +0900

    update

--- src/main/scala/lecture/ticket/Ticket.scala
+++ src/main/scala/lecture/ticket/Ticket.scala
@@ -2,8 +2,7 @@ package lecture.ticket
 
 abstract class Tree {
   def value: Double
-  def valueEquals(x: Double): Boolean = math.abs(x - value) < 1e-5
-  // def valueEquals(x: Double): Boolean = ???
+  def valueEquals(x: Double): Boolean = ???
 }
 
 case class Leaf(n: Int) extends Tree {
@@ -19,8 +18,8 @@ case class Node(op: String, left: Tree, right: Tree) extends Tree {
     case "/" => ???
   }
 
-  override def toString =
-    "(" + left.toString + " " + op + " " + right.toString + ")"
+  override def toString = ???
+
 }
 
 object Ticket {
